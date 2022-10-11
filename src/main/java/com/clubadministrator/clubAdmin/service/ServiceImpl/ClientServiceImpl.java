25ea42dd1f829b5a62bf386c36d18cc32d9482c0
LeandroVizgarra <leandro.elias.vizgarra@gmail.com>
2022-10-11 14:11:49 -0300

    Service added

---
+++ src/main/java/com/clubadministrator/clubAdmin/service/ServiceImpl/ClientServiceImpl.java
@@ -0,0 +1,46 @@
+package com.clubadministrator.clubAdmin.service.ServiceImpl;
+
+import com.clubadministrator.clubAdmin.dtos.ClientDTO;
+import com.clubadministrator.clubAdmin.entities.ClientEntity;
+import com.clubadministrator.clubAdmin.mapper.ClientMapper;
+import com.clubadministrator.clubAdmin.repository.ClientRepository;
+import com.clubadministrator.clubAdmin.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+    private final ClientMapper clientMapper;
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientServiceImpl(ClientMapper clientMapper, ClientRepository clientRepository) {
+        this.clientMapper = clientMapper;
+        this.clientRepository = clientRepository;
+    }
+
+    public ClientDTO createClient(ClientDTO clientDTO){
+        ClientEntity clientEntity = clientMapper.clientDTO2Entity(clientDTO);
+        ClientEntity clientSaved = clientRepository.save(clientEntity);
+        return clientMapper.clientEntity2DTO(clientSaved);
+    }
+
+    public String deleteClient(Long id){
+        ClientEntity entity = clientRepository.getById(id);
+        String name = entity.getFirstName();
+        clientRepository.deleteById(id);
+        return "Client "+name+" has been deleted";
+    }
+    public ClientDTO updateClient(Long id,ClientDTO dto){
+        ClientEntity entity = clientRepository.findById(id).orElseThrow(
+                ()-> new RuntimeException("No User Found"));
+        entity.setId(dto.getId());
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setStartDate(dto.getStartDate());
+        entity.setActiveUntilDate(dto.getActiveUntilDate());
+        entity.setIsActive(dto.getIsActive());
+        return clientMapper.clientEntity2DTO(entity);
+
+    }
+}
