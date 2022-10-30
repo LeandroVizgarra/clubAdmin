77f8309a1a67ba9bc9ffe38cf605a5f59f66b6d0
LeandroVizgarra <leandro.elias.vizgarra@gmail.com>
2022-10-30 20:15:27 -0300

    GET plan in controller

--- src/main/java/com/clubadministrator/clubAdmin/controller/PlanController.java
+++ src/main/java/com/clubadministrator/clubAdmin/controller/PlanController.java
@@ -1,8 +1,13 @@
 package com.clubadministrator.clubAdmin.controller;
 
 import com.clubadministrator.clubAdmin.service.PlanService;
+import com.clubadministrator.clubAdmin.utils.FinalPage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +18,11 @@ public class PlanController {
     public PlanController(PlanService planService) {
         this.planService = planService;
     }
-
-    //TODO continue controller.
+    @GetMapping
+    public ResponseEntity<FinalPage> getAll(@RequestParam(required = false,defaultValue = "1")int page,
+                                            @RequestParam(required = false) String name,
+                                            @RequestParam(required = false) Long price){
+        FinalPage plans = planService.getAllPlanByPage(page,name,price);
+        return new ResponseEntity<>(plans, HttpStatus.OK);
+    }
 }
